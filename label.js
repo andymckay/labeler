4e4f92e2fa950c9b71e1e70f855e1379ae76881d
Andy McKay <andymckay@github.com>
2019-09-04 16:40:10 -0700

    aargh

--- label.js
+++ label.js
@@ -1,8 +1,26 @@
 const github = require('@actions/github');
 const core = require('@actions/core');
 
-const myToken = core.getInput('myToken');
-const octokit = new github.GitHub(myToken);
-const context = github.context;
+async function label() {
+    const myToken = core.getInput('myToken');
+    const octokit = new github.GitHub(myToken);
+    const context = github.context;
 
-console.log(context);
+    await octokit.issues.update({
+        owner: context.payload.repository.owner.name,
+        repo: context.payload.repository.name,
+        number: context.payload.issue.number,
+        labels: ["needs-triage"]
+    });
+
+    return context.payload.issue.number;
+}
+
+label()
+   .then(
+      (number) => { console.log(`Labelled ${number}`) },
+      (err)  => { console.log(err) }
+   )
+   .then(
+       () => { process.exit() }
+    )
