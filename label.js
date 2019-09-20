6708e343fea12293aaf5dd06ec56ff55f4ea1349
Andy McKay <andymckay@github.com>
2019-09-20 16:03:32 -0700

    try that

--- label.js
+++ label.js
@@ -1,38 +1,40 @@
-const github = require('@actions/github');
-const core = require('@actions/core');
+const github = require("@actions/github");
+const core = require("@actions/core");
+
+const requested = core
+  .getInput("labels")
+  .split(",")
+  .map(x => x.trim());
 
 async function label() {
-    const myToken = core.getInput('repo-token');
-    const octokit = new github.GitHub(myToken);
-    const context = github.context;
+  const myToken = core.getInput("repo-token");
+  const octokit = new github.GitHub(myToken);
+  const context = github.context;
 
-    let labels = context.payload.issue.labels.map(label => label.name);
-    if (labels.includes["needs-triage"]) {
-        return false;
+  let labels = context.payload.issue.labels.map(label => label.name);
+  for (let requestedLabel of requested) {
+    if (labels.includes(requestedLabel)) {
+      labels.push(requestedLabel);
     }
-    labels.push("needs-triage");
-
-    await octokit.issues.update({
-        owner: context.payload.repository.owner.login,
-        repo: context.payload.repository.name,
-        issue_number: context.payload.issue.number,
-        labels: labels
-    });
-
-    return context.payload.issue.number;
+  }
+  await octokit.issues.update({
+    owner: context.payload.repository.owner.login,
+    repo: context.payload.repository.name,
+    issue_number: context.payload.issue.number,
+    labels: labels
+  });
+  return context.payload.issue.number;
 }
 
 label()
-   .then(
-      (result) => {
-          if (result) {
-              console.log(`Labelled ${result}`);
-          } else {
-              console.log(`Needs-triage already exists, no need to add.`);
-          }
-      },
-      (err)  => { console.log(err) }
-   )
-   .then(
-       () => { process.exit() }
-    )
+  .then(
+    result => {
+      console.log(`Labelled ${result} with ${requested}.`);
+    },
+    err => {
+      console.log(err);
+    }
+  )
+  .then(() => {
+    process.exit();
+  });
