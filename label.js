9a387bb4843df16e6f624eeb9bd06442f831e66f
Rachael Sewell <rachmari@github.com>
2020-05-24 14:48:06 -1000

    Ignore team members and enable pull requests (#1)

--- label.js
+++ label.js
@@ -19,15 +19,36 @@ async function label() {
   const context = github.context;
   const repoName = context.payload.repository.name;
   const ownerName = context.payload.repository.owner.login;
-  const issueNumber = context.payload.issue.number;
+  let issueNumber;
+  if (context.eventName === "issues") {
+    issueNumber = context.payload.issue.number;
+  } else {
+    issueNumber = context.payload.number;
+  }
+  const team = core.getInput("team");
 
   // query for the most recent information about the issue. Between the issue being created and
   // the action running, labels or asignees could have been added
-  var updatedIssueInformation = await octokit.issues.get({
+  const updatedIssueInformation = await octokit.issues.get({
     owner: ownerName,
     repo: repoName,
     issue_number: issueNumber
   });
+
+  if (team) {
+    const teamArr = team.split("/");
+    const org = teamArr.shift();
+    const teamSlug = teamArr.pop();
+    const teamMembers = await octokit.request(
+      `/orgs/${org}/teams/${teamSlug}/members`
+    );
+    const logins = teamMembers.data.map(member => member.login);
+    for (let login of logins) {
+      if (login === updatedIssueInformation.data.user.login) {
+        return `No action being taken. Ignoring because the author of the issue or pull request is part of the ${team} team`;
+      }
+    }
+  }
 
   if (ignoreIfAssigned) {
     // check if the issue has been assigned to anyone
